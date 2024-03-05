86e45eb36c871286d65ddd307073546a7d4dbc4f
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-03-05 09:53:23 -0300

    commit sql text

--- code_1.py
+++ code_1.py
@@ -67,13 +67,3 @@ for user in users:
 
 
 
-import random as rd
-import numpy as np
-
-dados = [(rd.randrange(10), rd.randrange(10), rd.randrange(10), rd.randrange(10)) for i in range(2)]
-
-a, b, c, d= dados[0]
-e, f, g, h = dados[1]
-
-dist =  np.sqrt(np.square(a - e) + np.square(b- f) + np.square(c - g) + np.square(d - h))
-print(dist)
--- firstcodedatascience.py
+++ firstcodedatascience.py
@@ -3,63 +3,21 @@
 from __future__ import division # divisao inteira esta incompleta
 
 
-users = [
-    {"id": 0, "name": "Breno"},
-    {"id": 1, "name": "ze"},
-    {"id": 2, "name": "chico"},
-    {"id": 3, "name": "elo"},
-    {"id": 4, "name": "ioga"},
-    {"id": 5, "name": "Bruna"},
-    {"id": 6, "name": "Brena"},
-    {"id": 7, "name": "Brena"},
-    {"id": 8, "name": "Batista"},
-    {"id": 9, "name": "Baiao"},
-]
+users = [{"id": 0, "name": "Hero"},
+         {"id": 1, "name": "Dunn"},
+         {"id": 2, "name": "Sue"},
+         {"id": 3, "name": "Chi"},
+         {"id": 4, "name": "Thor"},
+         {"id": 5, "name": "kate"},
+         {"id": 6, "name": "Clive"},
+         {"id": 7, "name": "hicks"},
+         {"id": 8, "name": "Devin"},
+         {"id": 9, "name": "klein"}]
 
 #criamos uma lista com logica de afiliação usando os indices
-friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
 (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
 #introduzimos uma nova lista no dicionario para correlacionar esses dados
-for user in users:
-    user["friends"] = []
-    print(user)
+friendships = {users[id] : [] for user in users }
 
-#iremos preencher essas listas, passara os valores de friendships dos pares ordenados para i e j
-    
-for i,j in friendships:
-    #usuario do dict com indice i no campo frinds recebe usuario do indice j
-    users[i]["friends"].append(users[j]["id"]) #adiciona i como amigo de j
-    users[j]["friends"].append(users[i]["id"]) #aciona j como amigo de i
-    
-
-#defininfo funcao para contagem de quanto amigos cada usuario tem
-def number_of_frinds(user):
-    #quantos amigos cada usuario tem
-    return len(user["friends"]) #conta tamanho da lista friends_ids
-
-total_connecions = sum(number_of_frinds(user) for user in users) #chama a funcao para todos os valores contindos no usuario
-
-
-# from __future__ import division # divisao inteira esta incompleta E CHAMADA AQUI
-
-num_users = len(users) #tamanho da lista de usuarios
-avg_connectins = total_connecions/num_users
-
-#criar uma lista e preenche ela para todos os valores de user
-num_frinds_by_id = [(user["id"], number_of_frinds(user)) for user in users]
-
-# gabiarra do codigo que tem no livro, feito por mim
-for x in user["id"], num_frinds_by_id:
-    
-    sorted(num_frinds_by_id,
-        key=lambda x: x,
-        reverse=True)
-    
-def friends_of_friends_ids_bad(user):
-    
-    for friend in user["friends"]:
-        print(friend)
-    for foaf in friend["friends"]:
-        print()
-        
